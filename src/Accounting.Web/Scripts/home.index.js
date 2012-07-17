4e3a8bd02ee26ce4e4499a143caaf8abe6caa726
lcranf <lcranf@gmail.com>
2012-07-17 00:14:44 -0400

    fixed bug where items can be null

--- src/Accounting.Web/Scripts/home.index.js
+++ src/Accounting.Web/Scripts/home.index.js
@@ -2,11 +2,16 @@
 /// <reference path="knockout-2.1.0.js" />
 
 
-var AccountIndexViewModel = function () {
+var AccountIndexViewModel = function() {
+    var self = this;
     this.accounts = ko.observableArray([]);
     this.selectedAccount = ko.observable();
     this.campaigns = ko.observableArray([]);
     this.selectedCampaign = ko.observable();
+
+    this.selectedAccountId = function() {
+        return self.selectedAccount() !== null ? self.selectedAccount().Id() : -1;
+    };
 }
 
 var viewModel = new AccountIndexViewModel();
